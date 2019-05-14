e279b39a651ddd2dcc36154a35a5dbc9764ebb0a
batisteta <34096625+batisteta@users.noreply.github.com>
2019-05-15 01:05:31 +0200

    Create Accueil.php

---
+++ Accueil.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Accueil</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+		</head>
+	<body>
+		<nav>
+			<ul> 
+				<li class="menu-accueil"><a href="accueil.php">Accueil</a></li>
+				<li class="menu-Recette"><a href="Nouveautes.php">Nos recettes</a>
+					<ul class="submenu">
+						<li><a href="Nouveautes.php">Tout savoir en video!</a></li>
+						<li><a href="Viande.php">Liste de recettes rapides</a></li>
+						<li><a href="Viande.php">Nos entrées</a></li>
+						<li><a href="Viande.php">Nos plats</a></li>
+						<li><a href="Dessert.php">Nos desserts</a></li>
+				</ul></li>
+				<li class="menu-contact"><a href="Contact.php">Nous Rejoindre</a></li>
+			</ul>
+		</nav>
+		<br/><br/>
+		<center><header>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+					<img src="Viande2.jpg" alt="viande2">
+					<img src="Viande1.jpg" alt="viande1">
+					<img src="viande.jfif" alt="viande">
+					<img src="Viande1.jpg" alt="viande1">
+				</div>
+		</header></center>
+		<br/><br/><br/><br/><br/><br/><br/>
+				<div>
+			<center><h1>Présentation</h1></center>
+			<center> <img src="fulb.png" alt="Lycee Fulbert"> </center>
+			<br/>
+				<form style="border:1px solid #32CD32">
+					<mark><p class="oblique">Bienvenue sur notre site !</p>
+
+					<p class="thick">Notre site à pour projet d'accompagner l'association "les amicale" du lycée fulbert situé à chartres</p>
+
+					<p class="oblique">Notre objectif :</p>
+					<p class="normal">	- Concevoir un site internet en respectant des normes bien précises.<br/>
+										- Améliorer nos compétences dans la programmation web.<br/>
+										- Savoir travailler en équipe lors d'un projet et améliorer notre autonomie.</p>
+
+					<p class="oblique">Pour qui ?</p>
+					<p class="normal">Nous concevons un site internet pour l'association "les amicale" basés au lycée fulbert et en partenariat avec Solibio.</p>
+					</mark>
+				</form>
+		</div>
+		
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		
+		<!-- footer !-->
+		<footer>
+			<div class="footer-main-div">
+		
+			<div class="footer-social-icons">
+				<ul>
+					<li><a href="https://www.facebook.com/solibio.eu/" target target="blank"><img src="logoFB.jpg"><i class="fa-facebook"></i></a></li>
+					<li><a href="https://www.solibio.eu/presentation-solibio" target target="blank"><img src="soli.jpg"><i class="fa fa-solibio"></i></a></li>
+					<li><a href="https://www.gralon.net/mairies-france/eure-et-loir/association-amicale-du-lycee-fulbert-chartres_W281005626.htm" target target="blank"><img src="mozi.jpg"><i class="fa fa-google-plus"></i></a></li>
+					<li><a href="https://www.youtube.com/watch?v=tJEVqqRPg3g" target target="blank"><img src="you.jpg"><i class="fa fa-youtube"></i></a></li>
+				</ul>
+			</div>
+			<div class="footer-menu-one">
+				<ul>
+					<li><a href="accueil.php">Accueil</a></li>
+					<li><a href="about.php">A propos de nous</a></li>
+					<li><a href="#">Nos recettes</a></li>
+					<li><a href="Contact.php">Nous Rejoindre</a></li>
+				</ul>
+			</div>
+			<div class="footer-menu-two">
+				<ul>
+					<li><a href="Viande.php">Commentaires</a></li>
+					<li><a href="Nouveautes.php">Nouveautés</a></li>
+					<li><a href="accueil.php">Galeries</a></li>
+				</ul>
+			</div>
+			<div class="footer-bottom">
+				<p>Design by: <a href="about.php">Baptiste Lacroix</a></p>
+			</div>
+		</footer>
+	</body>
+</html>
