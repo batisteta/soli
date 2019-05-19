46cc4f6562889e47f9265635a3663ed387f0b100
batisteta <34096625+batisteta@users.noreply.github.com>
2019-05-19 15:17:51 +0200

    Update Inscription.php

--- Inscription.php
+++ Inscription.php
@@ -1,60 +1,30 @@
 <?php
-$conn-mysqli_connect('localhost','root','','base_solebio') or die(mysqli_error());
-$NOM=$_POST['pseudo'];
-$EML=$_POST['mail'];
-$PWD=$_POST['pwd'];
+$bdd =new PDO('mysql:host=127.0.0.1;dbname=base_solebio;charset=utf8', 'root', '');
 
-$req="INSERT INTO utilisateurs (mail,mot_de_passe,pseudo) values ('$NOM','$EML','$PWD')";
-
-$res=mysqli_query($conn,$red);
+	if(!empty($_POST['pseudo'])AND !empty($_POST['mail'])AND !empty($_POST['mot_de_passe'])AND !empty($_POST['mdpR']))
+	{
+	$pseudo= htmlspecialchars($_POST['pseudo']);
+	$mail= htmlspecialchars($_POST['mail']);
+		$mot_de_passe= md5($_POST['mot_de_passe']);
+		$mdpR= md5($_POST['mdpR']);
+	if($Pseudo<= 255)
+		{
+		$requêtemail = $bdd->prepare("Select * From utilisateurs where mail=?");
+		$requêtemail ->execute(array($mail));
+		$mailexist =$requêtemail ->rowCount();
+			if($mailexist==0)
+			{
+			if($mdp== $mdpR)
+				{
+					$insertmbr = $bdd->prepare("INSERT INTO utilisateurs(pseudo, mot_de_passe, mail) Values(?, ?, ?)");
+					$insertmbr ->execute(array($pseudo, $mot_de_passe, $mail));
+					$_erreur="votre compte a bien été créé";
+				}
+			else
+				{
+					$_erreur="Les mots de passe ne correspondent pas";
+				}
+			}
+		}
+	}
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Inscription</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<nav>
-			<ul> 
-				<li class="menu-accueil"><a href="accueil.php">Accueil</a></li>
-				<li class="menu-Recette"><a href="Nouveautes.php">Nos recettes</a>
-					<ul class="submenu">
-						<li><a href="Nouveautes.php">Tout savoir en video!</a></li>
-						<li><a href="Viande.php">Liste de recettes rapides</a></li>
-						<li><a href="Viande.php">Nos entrées</a></li>
-						<li><a href="Viande.php">Nos plats</a></li>
-						<li><a href="Dessert.php">Nos desserts</a></li>
-				</ul></li>
-				<li class="menu-contact"><a href="Contact.php">Nous Rejoindre</a></li>
-			</ul>
-		</nav>
-		<br/><br/>
-		
-	<form >
-		<form style="border:1px solid #32CD32">
-			<div class="container">
-			<mark><h1>S'inscrire</h1></mark>
-				<hr>
-			<p style="color:green">	<label><b>Email</b></label>
-				<br /><?php echo ($EML)?></p>
-			<p style="color:green">	<label><b>Pseudo</b></label>
-				
-				<br /><?php echo ($NOM)?></p>
-			<p style="color:green">	<label><b>Mot de passe</b></label>
-				
-				<br /><?php echo ($PWD)?></p>
-			<p style="color:green">	<label><b>Répéter le mot de passe</b></label>
-				
-				<br /></p>
-				<label>
-				</label>
-				<a href="#" style="color:dodgerblue">Terms & Privacy</a>.
-				<hr>
-			<p><a href="Accueil.php">Retourner à l'accueil</a></p>
-			</div>
-		</form>
-	</form>
-	</body>
-	</html>
