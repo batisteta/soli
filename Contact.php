145a50339515d599693967f0b3587955c02818c5
batisteta <34096625+batisteta@users.noreply.github.com>
2019-05-19 15:16:10 +0200

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -12,33 +12,30 @@
 				<li class="menu-Recette"><a href="Nouveautes.php">Nos recettes</a>
 					<ul class="submenu">
 						<li><a href="Nouveautes.php">Tout savoir en video!</a></li>
-						<li><a href="Viande.php">Liste de recettes rapides</a></li>
-						<li><a href="Viande.php">Nos entrées</a></li>
-						<li><a href="Viande.php">Nos plats</a></li>
+						<li><a href="Entree.php">Nos entrées</a></li>
+						<li><a href="Nosplats.php">Nos plats</a></li>
 						<li><a href="Dessert.php">Nos desserts</a></li>
 				</ul></li>
 				<li class="menu-contact"><a href="Contact.php">Nous Rejoindre</a></li>
 			</ul>
 		</nav>
 		<br/><br/>
-		
-
-		<form method="POST" action="Inscription.php">
 		<form style="border:1px solid #32CD32">
 			<div class="container">
+			<form action="Inscription.php" method="post">
 			<mark><h1>S'inscrire</h1></mark>
 				<hr>
 			<p style="color:green">	<label><b>Email</b></label>
 				<input type="text" placeholder="Entrer un email" name="mail" required>
 				<br /></p>
 			<p style="color:green">	<label><b>Pseudo</b></label>
-				<input type="text" placeholder="Choisissez votre pseudo" name="name" required>
+				<input type="text" placeholder="Choisissez votre pseudo" name="pseudo" required>
 				<br /></p>
 			<p style="color:green">	<label><b>Mot de passe</b></label>
-				<input type="password" placeholder="Entrer un mot de passe" name="pwd" required>
+				<input type="password" placeholder="Entrer un mot de passe" name="mot_de_passe" required>
 				<br /></p>
 			<p style="color:green">	<label><b>Répéter le mot de passe</b></label>
-				<input type="password" placeholder="Repeter le mot de passe" name="rpwd" required>
+				<input type="password" placeholder="Repeter le mot de passe" name="mdpR" required>
 				<br /></p>
 				<label>
 			<p style="color:green">	<input type="checkbox" checked="Verifier" style="margin-bottom:15px"> Se rappeler de moi
@@ -51,10 +48,10 @@
 				  <button type="submit" class="signupbtn" value="Save">S'enregistrer</button>
 				</div>
 			</div>
-		</form>
+			</form>
 		</form>
 		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-		<!-- footer !-->
+				<!-- footer !-->
 		<footer>
 			<div class="footer-main-div">
 		
@@ -70,13 +67,13 @@
 				<ul>
 					<li><a href="accueil.php">Accueil</a></li>
 					<li><a href="about.php">A propos de nous</a></li>
-					<li><a href="#">Nos recettes</a></li>
+					<li><a href="Entree.php">Nos recettes</a></li>
 					<li><a href="Contact.php">Nous Rejoindre</a></li>
 				</ul>
 			</div>
 			<div class="footer-menu-two">
 				<ul>
-					<li><a href="Viande.php">Commentaires</a></li>
+					<li><a href="Nosplats.php">Commentaires</a></li>
 					<li><a href="Nouveautes.php">Nouveautés</a></li>
 					<li><a href="accueil.php">Galeries</a></li>
 				</ul>
