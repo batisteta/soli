67936055631a32f89adfcd068310b0396521335f
batisteta <34096625+batisteta@users.noreply.github.com>
2019-05-19 15:22:04 +0200

    Update Dessert.php

--- Dessert.php
+++ Dessert.php
@@ -12,20 +12,21 @@
 					<li class="menu-Recette"><a href="Nouveautes.php">Nos recettes</a>
 						<ul class="submenu">
 							<li><a href="Nouveautes.php">Tout savoir en video!</a></li>
-							<li><a href="Viande.php">Liste de recettes rapides</a></li>
-							<li><a href="Viande.php">Nos entrées</a></li>
-							<li><a href="Viande.php">Nos plats</a></li>
+							<li><a href="Entree.php">Nos entrées</a></li>
+							<li><a href="Nosplats.php">Nos plats</a></li>
 							<li><a href="Dessert.php">Nos desserts</a></li>
 					</ul></li>
 					<li class="menu-contact"><a href="Contact.php">Nous Rejoindre</a></li>
 				</ul>
 			</nav>
+		<br/>
 		<div>
 			<form style="border:2px solid #32CD32">
 			<mark><center><h1>Les Desserts</h1></center></mark>
 			</form>
 			<br/>
 			<center> <img src="creme.jpg" alt="crême brûlée"> </center>
+			<br/>
 				<form style="border:1px solid #32CD32">
 					<mark><p class="oblique">Etape 1 :</p>
 
@@ -57,7 +58,7 @@
 				<button type="submit" class="signupbtn">Envoyer un commentaire</button>
 			</div>
 			<br/>
-		<!-- footer !-->
+				<!-- footer !-->
 		<footer>
 			<div class="footer-main-div">
 		
@@ -65,7 +66,7 @@
 				<ul>
 					<li><a href="https://www.facebook.com/solibio.eu/" target target="blank"><img src="logoFB.jpg"><i class="fa-facebook"></i></a></li>
 					<li><a href="https://www.solibio.eu/presentation-solibio" target target="blank"><img src="soli.jpg"><i class="fa fa-solibio"></i></a></li>
-					<li><a href="https://www.gralon.net/mairies-france/eure-et-loir/association-amicale-du-lycee-fulbert-chartres_W281005626.htm" target target="blank"><img src="mozi.jpg"><i class="fa fa-google-plus"></i></a></li>
+					<li><a href="https://www.gralon.net/mairies-france/eure-et-loir/association-amicale-du-lycee-fulbert-chartres_W281005626.htm" target target="blank"><img src="mozi.png"><i class="fa fa-google-plus"></i></a></li>
 					<li><a href="https://www.youtube.com/watch?v=tJEVqqRPg3g" target target="blank"><img src="you.jpg"><i class="fa fa-youtube"></i></a></li>
 				</ul>
 			</div>
@@ -73,14 +74,14 @@
 				<ul>
 					<li><a href="accueil.php">Accueil</a></li>
 					<li><a href="about.php">A propos de nous</a></li>
-					<li><a href="#">Nos recettes</a></li>
+					<li><a href="Entree.php">Nos recettes</a></li>
 					<li><a href="Contact.php">Nous Rejoindre</a></li>
 				</ul>
 			</div>
 			<div class="footer-menu-two">
 				<ul>
-					<li><a href="Viande.php">Commentaires</a></li>
-					<li><a href="Dessert.php">Nouveautés</a></li>
+					<li><a href="Nosplats.php">Commentaires</a></li>
+					<li><a href="Nouveautes.php">Nouveautés</a></li>
 					<li><a href="accueil.php">Galeries</a></li>
 				</ul>
 			</div>
